5e12ca2da1c1d0f1ff33ce3ff45673953bb54b67
Pavan494876 <vtu29035@veltech.edu.in>
2025-10-29 18:40:09 +0530

    Delete 1a.py

--- 1a.py
+++
@@ -1,5 +0,0 @@
-p=int(input('Enter the principal amount:'))
-r=int(input('Enter the rate amount:'))
-t=int(input('Enter the time in years:'))
-CI=p*((1+r/100)**t)-p
-print('The compound interest is:',CI)
